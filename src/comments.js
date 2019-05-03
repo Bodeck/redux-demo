b87e0602db87fc6fdfa555f5b59fc1fdd706bf5d
Bogus Franczak <bfranczak@gmail.com>
2019-05-03 13:47:05 +0200

    Add reducers

---
+++ src/comments.js
@@ -0,0 +1,40 @@
+import { ADD_COMMENT, REMOVE_COMMENT, THUMB_DOWN_COMMENT, THUMB_UP_COMMENT, EDIT_COMMENT } from './actions';
+
+export default function comments(state = [], action) {
+  switch (action.type) {
+    case ADD_COMMENT:
+      return [{
+        id: action.commentId,
+        text: action.text,
+        votes: 0
+      }, ...state];
+    case REMOVE_COMMENT:
+      return state.filter((comment) => comment.id !== action.commentId);
+    case EDIT_COMMENT:
+      return state.map((comment) => {
+        if (comment.id === action.commentId) {
+          return Object.assign({}, comment, { text: action.text });
+        } else {
+          return comment;
+        }
+      });
+    case THUMB_UP_COMMENT:
+      return state.map((comment) => {
+        if (comment.id === action.commentId) {
+          return Object.assign({}, comment, { votes: comment.votes++ });
+        } else {
+          return comment;
+        }
+      });
+    case THUMB_DOWN_COMMENT:
+      return state.map((comment) => {
+        if (comment.id === action.commentId) {
+          return Object.assign({}, comment, { votes: comment.votes-- });
+        } else {
+          return comment;
+        }
+      });
+    default:
+      return state;
+  }
+};
