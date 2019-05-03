bf724194e76b29c7db4b4087392a856a4202dacc
Bogus Franczak <bfranczak@gmail.com>
2019-05-03 13:42:53 +0200

    Add components

---
+++ src/CommentsList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Comment from './Comment';
+
+const CommentsList = ({comments}) => {
+  return (
+    <ul>{comments.map((comment) => <Comment key={comment.id} {...comment} />) }</ul>
+  )
+}
+
+export default CommentsList;
